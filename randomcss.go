6868ed9b0fd5533f751db20f5053154e2c91b7b3
lucien <git@lucienr.com>
2024-04-14 17:56:30 +0000

    added cli options and url parameters

--- randomcss.go
+++ randomcss.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-        "fmt"
-        "math/rand"
-        "net/http"
-        "os"
-        "time"
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
-var root string = "/var/www/lucienr.com/"
+var root *string = flag.String("r", "/path/to/root", "The root directory to serve files from. Default is /path/to/root")
 
 func randomcss(w http.ResponseWriter, r *http.Request) {
-        n := rand.Intn(30)
-        cssFile := fmt.Sprintf("%s/styles.css.%d", root, n)
-        file, err := os.Open(cssFile)
-        if err != nil {
-                http.Error(w, "File not found.", 404)
-                return
-        }
-        defer file.Close()
+	uq := r.URL.Query().Get("n")
+	var n int
+	if uq == "" {
+		n = rand.Intn(30)
+	} else {
+		var err error
+		n, err = strconv.Atoi(uq)
+		if err != nil {
+			http.Error(w, "Invalid parameter.", 400)
+			return
+		}
+	}
+	cssFile := fmt.Sprintf("%s/styles.css.%d", *root, n)
+	file, err := os.Open(cssFile)
+	if err != nil {
+		http.Error(w, "File not found.", 404)
+		return
+	}
+	defer file.Close()
 
-        http.ServeContent(w, r, "styles.css", time.Unix(0, 0), file)
+	http.ServeContent(w, r, "styles.css", time.Unix(0, 0), file)
 }
 
 func main() {
-        http.HandleFunc("/styles.css", randomcss)
-        http.Handle("/", http.FileServer(http.Dir(root)))
-        http.ListenAndServe("127.0.0.1:3332", nil)
+	port := flag.String("p", "3332", "The port to listen on. Default is 3332")
+	flag.Parse()
+	http.HandleFunc("/styles.css", randomcss)
+	http.Handle("/", http.FileServer(http.Dir(*root)))
+	http.ListenAndServe("127.0.0.1:"+*port, nil)
 }
